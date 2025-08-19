c1c1715d981f0a082c6a132ac8e1594430f9cb37
Med <cheriet-mohamed@hotmail.com>
2025-08-19 09:49:37 +0100

    updates

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -o errexit
+
+# Install Tesseract
+apt-get update
+apt-get install -y tesseract-ocr tesseract-ocr-eng
+
 # Install dependencies
 pip install -r requirements.txt
 
