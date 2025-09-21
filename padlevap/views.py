f837715209cce82d21d23914ab133d30506d5783
Med <cheriet-mohamed@hotmail.com>
2025-09-21 10:07:26 +0100

    updates

--- padlevap/views.py
+++ padlevap/views.py
@@ -278,6 +278,38 @@ class EmailLetterPostGlobal(mixins.ListModelMixin,
 
 
 
+class EmailLetterPostid(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return EmailLetter.objects.get(pk=pk)
+        except EmailLetter.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = EmailLetterSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = EmailLetterSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+  
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
 # show user
 
 class UserDetailsView(APIView):
