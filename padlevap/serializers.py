925f1491617de1acd7aa585467bddb1b9430d7a4
cheriet-med <cheriet-mohamed@hotmail.com>
2025-11-18 14:40:28 +0100

    updates

--- padlevap/serializers.py
+++ padlevap/serializers.py
@@ -199,47 +199,12 @@ class EmailSerializer(serializers.ModelSerializer):
 
 
 class TestSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()  # Add this to get full URL
     
     class Meta:
         model = test
         fields = '__all__'
     
-    def get_image_url(self, obj):
-        """Return the full Cloudinary URL"""
-        if obj.image_en:
-            return CloudinaryImage(str(obj.image_en)).build_url()
-        return None
-    
-    def create(self, validated_data):
-        image_file = validated_data.get('image_en')
-        
-        if image_file:
-            # Extract original filename
-            original_filename = os.path.splitext(image_file.name)[0]
-            clean_filename = slugify(original_filename)
-            unique_id = str(uuid.uuid4())[:8]
-            public_id = f"review-images/{clean_filename}-{unique_id}"
-            
-            try:
-                upload_result = cloudinary.uploader.upload(
-                    image_file,
-                    public_id=public_id,
-                    folder="review-images",
-                    overwrite=False
-                )
-                
-                # The URL will now contain your original filename
-                print(f"Uploaded URL: {upload_result['secure_url']}")
-                # This will be something like:
-                # https://res.cloudinary.com/your-cloud/image/upload/v1234567890/review-images/nextjs-icon-abc12345.jpg
-                
-                validated_data['image_en'] = upload_result['public_id']
-                
-            except Exception as e:
-                raise serializers.ValidationError(f"Image upload failed: {str(e)}")
-        
-        return super().create(validated_data)
+  
 
 
 
